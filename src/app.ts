1e2519e78f71417ae7fb15d2d57aec46c3869239
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-06-01 21:47:49 +0600

    user controller and service

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userController from './app/modules/users/user.controller'
 
 const app: Application = express()
 
@@ -9,7 +10,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+app.use('/api/v1/users', userController.createUser)
+
+// testing
+app.get('/', async (req: Request, res: Response) => {
   res.send('Working successfully')
 })
 
