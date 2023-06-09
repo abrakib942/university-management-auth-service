2ea9b57b449e3dcb4a26cf216cc778c22c6ef861
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-06-10 00:12:48 +0600

    [Done] create academic semester

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-import cors from 'cors'
-import express, { Application } from 'express'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/users/user.routes'
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/user/user.routes';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.routes';
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
 //parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', UserRoutes)
+app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/academic-semester', AcademicSemesterRoutes);
 
 //Testing
 // app.get('/', async (req, res, next) => {
@@ -19,6 +21,6 @@ app.use('/api/v1/users', UserRoutes)
 // })
 
 //global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
